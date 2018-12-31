272e1da0f263ca0edd5b7dee7e0e3f3d00358187
liz <toyk1ng@qq,com>
2018-12-31 14:23:27 +0800

    去除测试环境，加入Jenkins自动打包框架

--- app/src/main/java/com/codetroupes/jenkinstestapp/MainActivity.java
+++ app/src/main/java/com/codetroupes/jenkinstestapp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.codetroupes.jenkinstestapp;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.test_text);
+        text.setText("提交版本自动打包测试:3\r\n代码版本号："+packageCode(this)+"\r\n程序版本号："+packageName(this));
+
     }
+
+    public static int packageCode(Context context) {
+        PackageManager manager = context.getPackageManager();
+        int code = 0;
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            code = info.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return code;
+    }
+
+    public static String packageName(Context context) {
+        PackageManager manager = context.getPackageManager();
+        String name = null;
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            name = info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return name;
+    }
+
+
 }
